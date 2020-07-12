ae0b5f69689cf5125d7ce02aa1b503eeed5261a4
marcoslacroix <marcoslacroix@gmail.com>
2020-07-12 20:40:05 -0300

    H2 database, test profile, JPA

--- src/main/java/com/example/projectspring/entities/User.java
+++ src/main/java/com/example/projectspring/entities/User.java
@@ -2,8 +2,16 @@ package com.example.projectspring.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
