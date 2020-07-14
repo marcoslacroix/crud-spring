5ff57afc344aa9b4408aa1784398142b4397900b
marcoslacroix <marcoslacroix@gmail.com>
2020-07-14 13:48:33 -0300

    part 1

---
+++ src/main/java/com/example/projectspring/entities/pk/OrderItemPK.java
@@ -0,0 +1,55 @@
+package com.example.projectspring.entities.pk;
+
+import com.example.projectspring.entities.Order;
+import com.example.projectspring.entities.Product;
+
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import java.io.Serializable;
+
+@Embeddable
+public class OrderItemPK implements Serializable {
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof OrderItemPK)) return false;
+
+        OrderItemPK that = (OrderItemPK) o;
+
+        if (!getOrder().equals(that.getOrder())) return false;
+        return getProduct().equals(that.getProduct());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getOrder().hashCode();
+        result = 31 * result + getProduct().hashCode();
+        return result;
+    }
+}
